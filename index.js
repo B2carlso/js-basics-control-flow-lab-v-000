80959aedb647a98b83af4d9f87799b6f403873f0
Brad Carlson <b2carlso@yahoo.com>
2018-04-11 19:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const scuberGreetingForFeet(feet)
   if (feet <= 400){
     return "This one is on me!"
   }
+  else if (feet > 2000) {
+    if (feet > 2500) {
+      return "No can do."
+    }
+    else {
+      return 'I will gladly take your thirty bucks.'
+    }
+  }
